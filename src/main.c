33ea1b8127a3c4abbd252c9458fecf465de6da1e
William Ellett <williamjellett@gmail.com>
2022-11-01 22:50:38 +0100

    Add additional tests points
    Points added can be used to develop unit tests

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 /** @file
  *  @brief Main file.
  *  @description Just contains some example code. Adapt it in the way you like.
+ *  @note Test points used here can be used to develop unit tests
  */
 
 #include <stdio.h>
@@ -12,22 +13,42 @@
 #include "modules/sensors.h"
 
 int main(int argc, char *argv[]) {
-  adc_value_t value = 0;
+    adc_value_t value = 0;
+    printf("[MAIN] Initialising\n");
+    error_led_init();
 
-  error_led_init();
-  
-  sensors_init();
+    sensors_init();
 
-  adc_read_set_output(ADC_CHANNEL0, 205, ADC_RET_OK);
-  adc_read_set_output(ADC_CHANNEL1, 286, ADC_RET_OK);
+    printf("[MAIN] Test point 5 deg\n");
+    adc_read_set_output(ADC_CHANNEL0, 205, ADC_RET_OK);
+    adc_read_set_output(ADC_CHANNEL1, 286, ADC_RET_OK);
+    sensors_task();
 
-  sensors_task();
+    printf("[MAIN] Test point 5 deg\n");
+    sensors_task();
 
-  adc_read_set_output(ADC_CHANNEL0, 716, ADC_RET_OK);
+    printf("[MAIN] Test point 7 deg\n");
+    adc_read_set_output(ADC_CHANNEL0, 246, ADC_RET_OK);
+    adc_read_set_output(ADC_CHANNEL1, 319, ADC_RET_OK);
+    sensors_task();
 
-  sensors_task();
+    printf("[MAIN] Test point 30 deg\n");
+    adc_read_set_output(ADC_CHANNEL0, 716, ADC_RET_OK);
+    adc_read_set_output(ADC_CHANNEL1, 695, ADC_RET_OK);
+    sensors_task();
 
-  error_led_set(true);
+    printf("[MAIN] Test point divergant sensors\n");
+    adc_read_set_output(ADC_CHANNEL0, 409, ADC_RET_OK);
+    sensors_task();
+
+    printf("[MAIN] Test point divergant sensors\n");
+    sensors_task();
+
+    printf("[MAIN] Test point sensor failure\n");
+    adc_read_set_output(ADC_CHANNEL0, 409, ADC_RET_NOK);
+    sensors_task();
+
+    error_led_set(true);
 }
 
 
